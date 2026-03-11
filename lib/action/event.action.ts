0895d03f8354eebd4595bb3c3695129b49173264
Emmaprogram-on <153019512+Emmaprogram-on@users.noreply.github.com>
2026-03-11 11:47:59 +0100

    fix: null check for event

--- lib/action/event.action.ts
+++ lib/action/event.action.ts
@@ -1,15 +1,22 @@
-'use server';
+"use server";
 
 import { Event } from "@/database/event.model";
 import { connectToDatabase } from "@/lib/mongodb";
 
-export const getSimilarEventsBySlug = async (slug: string) => { 
-    try {
-        await connectToDatabase();
-        const event = await Event.findOne({ slug });
+export const getSimilarEventsBySlug = async (slug: string) => {
+  try {
+    await connectToDatabase();
+    const event = await Event.findOne({ slug });
 
-        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
-    } catch {
-        return [];
+    if (!event) {
+      return [];
     }
-}
+
+    return await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags },
+    }).lean();
+  } catch {
+    return [];
+  }
+};
