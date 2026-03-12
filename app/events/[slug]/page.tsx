200d7422f0813c7006dd0e84a6db8a8a8a8bea9d
Emmaprogram-on <153019512+Emmaprogram-on@users.noreply.github.com>
2026-03-12 13:28:14 +0100

    dynamic rendering for slug page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -5,11 +5,17 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> 
   const { slug } = await params;
   return (
     <main>
-      <Suspense fallback={<div>Loading event details...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <EventDetails slug={slug} />
       </Suspense>
     </main>
   )
 }
 
-export default EventDetailsPage
+export async function generateStaticParams() {
+  return [];
+}
+
+export const dynamic = 'force-dynamic';
+
+export default EventDetailsPage;
